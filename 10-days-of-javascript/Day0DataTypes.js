bc3a728d0aa6b1c1ebfe7ced4ae0c1238cfccf9e
Jon <77033622+jon-cbar@users.noreply.github.com>
2021-02-18 11:40:37 -0300

    Create Day0DataTypes.js

---
+++ 10-days-of-javascript/Day0DataTypes.js
@@ -0,0 +1,9 @@
+function Day0DataTypes(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank ';
+
+    console.log(firstInteger + parseInt(secondInteger));
+    console.log(firstInteger + parseFloat(secondDecimal));
+    console.log(firstString + secondString);
+}
