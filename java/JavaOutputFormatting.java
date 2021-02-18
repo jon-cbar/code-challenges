48988162bf4485466e8993c0da2ac982389487ea
Jon <77033622+jon-cbar@users.noreply.github.com>
2021-02-18 11:29:36 -0300

    Update JavaOutputFormatting.java

--- java/JavaOutputFormatting.java
+++ java/JavaOutputFormatting.java
@@ -1,5 +1,26 @@
 public class JavaOutputFormatting {
     public static void main(String[] args) {
-        // TODO code here
+        int amountInputs = 3;
+        String key = "";
+        Integer value = 0;
+        String column1 = "";
+        String column2 = "";
+        String border = "================================";
+
+        System.out.println(border);
+
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < amountInputs; i++) {
+            key = scanner.next();
+            value = scanner.nextInt();
+
+            column1 = String.format("%-15s", key);
+            column2 = String.format("%03d", value);
+
+            System.out.println(column1 + column2);
+        }
+        scanner.close();
+
+        System.out.println(border);
     }
 }
