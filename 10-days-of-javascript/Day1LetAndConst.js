3c3c6383e8d34ba33b6894a1ef84f359ef9b2e4f
Jon <77033622+jon-cbar@users.noreply.github.com>
2021-02-18 12:05:18 -0300

    Create Day1LetAndConst.js

---
+++ 10-days-of-javascript/Day1LetAndConst.js
@@ -0,0 +1,8 @@
+function letAndConst() {
+    const PI = Math.PI;
+    const r = readLine();
+    let area = PI * Math.pow(r, 2);
+    let perimeter = 2 * PI * r;
+    console.log(area);
+    console.log(perimeter);
+}
